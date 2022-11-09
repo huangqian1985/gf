c87157bb5001398fcc86a5d05cab9c075a76973e
huangqian <huangqian1985@qq.com>
2022-11-09 19:45:25 +0800

    improve gproc.go code coverage

--- os/gproc/gproc_z_unit_test.go
+++ os/gproc/gproc_z_unit_test.go
@@ -9,11 +9,10 @@
 package gproc_test
 
 import (
-	"testing"
-
 	"github.com/gogf/gf/v2/os/gctx"
 	"github.com/gogf/gf/v2/os/gproc"
 	"github.com/gogf/gf/v2/test/gtest"
+	"testing"
 )
 
 func Test_ShellExec(t *testing.T) {
@@ -26,5 +25,48 @@ func Test_ShellExec(t *testing.T) {
 	gtest.C(t, func(t *gtest.T) {
 		_, err := gproc.ShellExec(gctx.New(), `NoneExistCommandCall`)
 		t.AssertNE(err, nil)
+	})
+}
+
+func TestPid(t *testing.T) {
+	gtest.C(t, func(t *gtest.T) {
+		t.AssertGE(gproc.Pid(), 0)
+	})
+}
+
+func TestPPid(t *testing.T) {
+	gtest.C(t, func(t *gtest.T) {
+		t.AssertGE(gproc.PPid(), 0)
+		t.Assert(gproc.SetPPid(0), nil)
+		t.AssertGT(gproc.PPid(), 0)
+		t.Assert(gproc.SetPPid(12345), nil)
+		t.Assert(gproc.PPid(), 12345)
+	})
+}
+
+func TestPPidOS(t *testing.T) {
+	gtest.C(t, func(t *gtest.T) {
+		t.AssertGE(gproc.PPidOS(), 0)
+	})
+}
+
+func TestIsChild(t *testing.T) {
+	gtest.C(t, func(t *gtest.T) {
+		t.Assert(gproc.IsChild(), true)
+	})
+}
+
+func TestStartTime(t *testing.T) {
+	gtest.C(t, func(t *gtest.T) {
+		t.AssertGE(gproc.StartTime(), 0)
+		t.AssertGE(gproc.Uptime(), 0)
+	})
+}
+
+func TestSearchBinary(t *testing.T) {
+	gtest.C(t, func(t *gtest.T) {
+		filePath := "./gproc.go"
+		t.Assert(gproc.SearchBinary("./gproc.go"), filePath)
+		t.Assert(gproc.SearchBinary("./noexist.go"), "")
 	})
 }
